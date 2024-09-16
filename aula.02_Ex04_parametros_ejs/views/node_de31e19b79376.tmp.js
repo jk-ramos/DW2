34fd079662a72ad1103066e9608ca3d891785fcc
Jak Ramos <142371206+jk-ramos@users.noreply.github.com>
2024-09-16 17:11:24 -0300

    Add files via upload

---
+++ aula.02_Ex04_parametros_ejs/views/node_de31e19b79376.tmp.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vídeos</title>
+</head>
+<body>
+    <h1>Bem-vindo a página de vídeos</h1>
+    <hr>
+    <h3>Esta é a página de vídeos</h3>
+    <p>Escola uma playlist ou o vídeo que deseja reproduzir...</p>
+</body>
+</html>
